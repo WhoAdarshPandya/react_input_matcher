7bc5e9fe0cdd31da0baeff0f213af9fa7fa567c3
WhoAdarshPandya <adarshpandya0@gmail.com>
2020-12-28 16:28:22 +0530

    bug fix : val instead of input2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [input1, setInput1] = useState("");
+  const [input2, setInput2] = useState("");
+  const [isMatched, setIsmatched] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleInput1 = (val) => {
+    setInput1(val);
+    if (val !== "" && input2 !== "") {
+      setIsVisible(true);
+      if (input2 === val) {
+        setIsmatched(true);
+      } else {
+        setIsmatched(false);
+      }
+    } else {
+      setIsVisible(false);
+    }
+  };
+  const handleInput2 = (val) => {
+    setInput2(val);
+    // ! error solved : putting val instead of input2
+    if (input1 !== "" && val !== "") {
+      setIsVisible(true);
+      if (input1 === val) {
+        setIsmatched(true);
+      } else {
+        setIsmatched(false);
+      }
+    } else {
+      setIsVisible(false);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>text matcher</h1>
+      <br />
+      <input
+        value={input1}
+        type="text"
+        placeholder="enter text1"
+        onChange={(e) => handleInput1(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={input2}
+        placeholder="enter text2"
+        onChange={(e) => handleInput2(e.target.value)}
+      />
+      {isVisible ? (
+        isMatched ? (
+          <p>text is matched</p>
+        ) : (
+          <p style={{ color: "red" }}> text is not matched</p>
+        )
+      ) : null}
     </div>
   );
 }
